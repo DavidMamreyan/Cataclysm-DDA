156625937cd1dcd4c2bc7f1f3917f675d3370b31
MaxxPC <david.008@bk.ru>
2026-08-30 17:11:33 +0700

    Show weapon category in item info (#84124)

--- src/item.cpp
+++ src/item.cpp
@@ -5782,6 +5782,16 @@ void item::properties_info( std::vector<iteminfo> &info, const iteminfo_query *p
         }
     }
 
+    if( parts->test( iteminfo_parts::DESCRIPTION_WEAPON_CATEGORY ) &&
+        !typeId()->weapon_category.empty() ) {
+        const std::string weapon_category_list = enumerate_as_string( typeId()->weapon_category,
+        []( const weapon_category_id & category ) {
+            return category->name().translated();
+        }, enumeration_conjunction::and_ );
+        info.emplace_back( "BASE", string_format( _( "* This item can be used as <good>%s</good>." ),
+                           weapon_category_list ) );
+    }
+
     if( parts->test( iteminfo_parts::DESCRIPTION_FLAGS ) ) {
         // concatenate base and acquired flags...
         cata::flat_set<flag_id> flags;
